697d5f7cd8ac91ca488ead87fa072af500ff388d
danie <danielcardona0110@outlook.com>
2023-10-20 13:10:19 -0500

    Estilos diferentes

--- src/Components/Organisms/Form/Forms.jsx
+++ src/Components/Organisms/Form/Forms.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 import Number from "../../Atoms/Number/Number";
 import Select from "../../Atoms/Select/Select";
-import LoadForm from "../../Molecules/LoadForm/LoadForm";
 import { Submit } from "../../Atoms/Submit/Submit";
 
 export default function Forms(props) {
@@ -22,7 +21,20 @@ export default function Forms(props) {
         </>
         <>
           <label>Load:</label>
-          <LoadForm fpClassname="fp" {...props} />
+          <Number onChange={props.onChangeLoad} placeholder="Carga" min={1} />
+          <Select
+            onChange={props.onChangeLoadType}
+            options={props.loadTypeOptions}
+          />
+          {props.factor ? <label> fp: </label> : null}
+          {props.factor ? (
+            <Number
+              onChange={props.onChangeFp}
+              placeholder="fp"
+              min={0.01}
+              max={1}
+            />
+          ) : null}
         </>
         <>
           <label>Voltage:</label>
